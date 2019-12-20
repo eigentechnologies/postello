db746d9f2118b1376950424a849ab4b6f28119af
Sergio Sofia <sergio.sofia@eigentech.com>
2019-12-20 15:57:43 +0000

    style welcome views

--- src/Views/welcome/index.js
+++ src/Views/welcome/index.js
@@ -1,14 +1,28 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+import HeaderBg from '../../svg/welcome-bg.svg'
+import WelcomeCTA from '../../svg/welcome-cta.svg'
+import PostelloLogo from '../../svg/postello-logo.svg'
+import EigenLogo from '../../svg/eigen-logo.svg'
+
+import './welcome.sass';
+
 function WelcomeView() {
   return (
-    <div>
-      POSTELLO says HELLO
-      <span role="img" aria-label="wave">
-        👋
-      </span>
-      <Link to="/camera">go to camera</Link>
+    <div className="main">
+      <div style={{backgroundImage:`url(${HeaderBg})`}} className="header-bg" />
+      <div className="welcome-header">
+        <div style={{backgroundImage:`url(${PostelloLogo})`}} className="header-logo" />
+      </div>
+      <div className="welcome-body">
+        <div className="quote">Take a picture and send an Owl!</div>
+        <Link to="/camera" className="welcome-link" style={{backgroundImage:`url(${WelcomeCTA})`}}/>
+      </div>
+      <div className="welcome-footer">
+        <span>Powered by</span>
+        <div style={{backgroundImage:`url(${EigenLogo})`}} className="eigen-logo" />
+      </div>
     </div>
   );
 }
