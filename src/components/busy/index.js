3544f8dc6ecbd4448e6352241d2fbbac439f6dba
Domenico Flauto <domenico.flauto@eigentech.com>
2019-12-20 17:16:44 +0000

    busy sign

--- src/components/busy/index.js
+++ src/components/busy/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
 import './busy.sass';
+import wait from '../../svg/waiting.svg';
 
 function Busy() {
   return(
     <div className="busy-sign">
-      <div className="busy-img" style={{backgroundImage: `url(${x})`}}/>>
+      <div className="busy-img" style={{backgroundImage: `url(${wait})`}}/>
     </div>
   )
 }
